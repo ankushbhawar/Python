49f2535d86e06a3f09b5488b1f8469e4d5909f92
ankushbhawar <bhawarankush2@gmail.com>
2021-03-24 19:29:09 +0530

    variables and basic data-types in python

---
+++ 01-variables.py
@@ -0,0 +1,28 @@
+''' variables => variable is a name given to a memory location in a program
+or container to store a values.'''
+
+# keywords => resurved words in a python.
+# identifiers => class/functions or variable names.
+
+''' data types => python is a fantastic language that automatically 
+identifies the type of a data for us.
+ ( integers, floating point numbers, strings, boolean, none )
+'''
+
+variable_1 = "Ankush"
+print(variable_1, type(variable_1))
+
+variable_2 = 5
+print(variable_2, type (variable_2))
+
+variable_3 = 1.4
+print(variable_3, type (variable_3))
+
+variable_4 = True
+print(variable_4, type(variable_4))
+
+variable_5 = False
+print(variable_5, type(variable_5))
+
+variable_6 = None
+print(variable_6, type(variable_6))
