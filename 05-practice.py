ef5cbc37c46da26e5b60119658083272edd7f988
ankushbhawar <bhawarankush2@gmail.com>
2021-03-24 23:35:09 +0530

    coding practice set 1

---
+++ 05-practice.py
@@ -0,0 +1,27 @@
+# add two numbers
+print("Program 1 : Addition of two numbers")
+a = input("Enter first number : ")
+a = int(a)
+b = input("Enter second number : ")
+b = int(b)
+print("addition of two numbers : ", a + b)
+
+# find a reminder when a number is divided by 2
+print("Program 2 : remindern when number is divided by 2")
+c = input("Enter a number : ")
+c = int(c)
+print("the reminder when ", c, "is divided by 2 is : ", c%2)
+
+# find average of two numbers entered by the users
+print("Program 3 : find average of two numbers")
+d = input("Enter first number : ")
+d = int(d)
+e = input("Enter second number : ")
+e = int(e)
+print("average of ", d, " and ", e, " is : ",  (d + e) / 2)
+
+# calculate square of a number entered by the user
+print("Program 4 : calculate square of a number entered by the user")
+f = input("Enter a number : ")
+f = int(f)
+print("square of ", f, " is : ", f * f)
