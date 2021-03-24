966b10512d5c64f3369b00e69b69ec78e8f6f1ae
ankushbhawar <bhawarankush2@gmail.com>
2021-03-24 20:16:06 +0530

    operators in python programming

---
+++ 02-operators.py
@@ -0,0 +1,42 @@
+# arithmetic operators
+print("arithmetic operators")
+print("1 + 3 = ", 1 + 3)
+print("4 - 2 = ", 4 - 2)
+print("1 * 11 = ", 1 * 11)
+print("5 / 2 = ", 5 / 2)
+
+# assignment operators 
+print("assignment operators")
+a = 14
+print(a)
+
+a += 2
+print("14 + 2 = ", a)
+
+a -= 11
+print("16 - 11 = ", a)
+
+a *= 5
+print("5 * 5 = ", a)
+
+a /= 2
+print("25 / 2 = ", a)
+
+# comparison operators
+print("comparison operators")
+print("2 > 4 : ", 2 > 4)
+print("2 < 4 : ", 2 < 4)
+print("5 >= 5 : ", 5 >= 5)
+print("5 <= 5 : ", 5 <= 5)
+print("2 == 2 : ", 2 == 2)
+print("2 != 2 : ", 2 != 2)
+
+# logical operators ( operates on a booleans)
+print("logical operators")
+boolean_1 = True
+boolean_2 = False
+
+print(boolean_1, " and", boolean_2, " : ", (boolean_1 and boolean_2))
+print(boolean_1, " or", boolean_2, " : ", (boolean_1 or boolean_2))
+print("not ", boolean_1, " : ", (not boolean_1))
+print("not ", boolean_2, " : ", (not boolean_2))
