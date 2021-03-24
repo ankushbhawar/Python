a32b996d938a2005feca447c3e8c4e8434120678
ankushbhawar <bhawarankush2@gmail.com>
2021-03-24 20:56:47 +0530

    type casting in python programming

---
+++ 03-typeCasting.py
@@ -0,0 +1,44 @@
+# type casting
+# a numbers can converted into a string and voice versa(if possible)
+
+print("string to integer conversion")
+a = "5" # string literal
+print(a, type (a))
+
+a = int(a) # numaric literal
+print(a, type (a))
+
+print("string to float conversion")
+b = "5"
+print(a, type (b))
+
+b= float(b)
+print(b, type (b))
+
+print("integer to string conversion")
+c = 13
+print(c, type(c))
+
+c = str(c)
+print(c, type(c))
+
+print("integer to float conversion")
+d = 14
+print(d, type(d))
+
+d = float(d)
+print(d, type(d))
+
+print("float to string conversion")
+e = 10.5
+print(e, type(e))
+
+e = str(e)
+print(e, type(e))
+
+print("float to integer conversion")
+f = 5.3
+print(f, type(f))
+
+f = int(f)
+print(f, type(f))
