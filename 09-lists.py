ec230e231c9aea39c146789d932dbe53c942f8b7
ankushbhawar <bhawarankush2@gmail.com>
2021-03-26 22:43:43 +0530

    lists in python programming

---
+++ 09-lists.py
@@ -0,0 +1,12 @@
+# python lists are containers to store a set of values of any data type
+a = [1, 3, 5, 4, 2]
+print(a)
+
+print(a[0])
+
+a[0] = 11
+print(a)
+
+# create a list of difrent data types
+b = ["hi", 1, 1.1, False]
+print(b)
