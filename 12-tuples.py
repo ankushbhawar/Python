28212eae0fd4d284a7bbc16b541362b77011da61
pythoneer-in <pythoneer.in@gmail.com>
2021-05-15 08:05:16 -0700

    tuples in python

---
+++ 12-tuples.py
@@ -0,0 +1,11 @@
+# creating a tuple useing ()
+t = (1,2,3,4,5)
+print(t)
+print(t[0])
+
+# t1 = () # empty tuple
+# t1 = (1) # wrong way to declare a tuple with single element
+t1 = (1,) # tuple with single element
+print(t1)
+
+# t[0] = 34 # cannot update
