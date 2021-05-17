d34db7f21323809ed2013d45a88c17399f1a80a6
pythoneer-in <pythoneer.in@gmail.com>
2021-05-16 21:11:24 -0700

    dictionary in python

---
+++ 14-dictionary.py
@@ -0,0 +1,16 @@
+myDict = {
+    "Fast" : "In a Quick Manner",
+    "Ankush" : "A Coder",
+    "Marks" : [1,5,3,4],
+    "anotherDict" : {
+        "Ankush" : "Developer",
+        "Hey" : "Ankush"
+    }
+}
+# print(myDict)
+# print(myDict["Ankush"])
+# myDict["Marks"].sort()
+myDict["Marks"] = [1]
+print(myDict["Marks"])
+# print(myDict["anotherDict"]["Ankush"])
+print(myDict["anotherDict"])
