ee9a392346c6c80114b7243526fdd144d757977e
pythoneer-in <pythoneer.in@gmail.com>
2021-05-18 02:33:36 -0700

    conditionals in python

---
+++ 18-conditionals.py
@@ -0,0 +1,30 @@
+# a = 5
+# if(a>3):
+#     print("The vealue of a is greator than 3")
+# elif(a>7):
+#     print("the vaue of a is greator than 7")
+# else:
+#     print("The value of a is not greator than 3 or 7")
+
+# age = int(input("Enter your age "))
+# if(age>18):
+#     print("Yes")
+# else:
+#     print("no")
+
+# b = 8
+# if(b >= 5 and b < 11):
+#     print("5 to 10")
+# elif(b < 5):
+#     print("less than 5")
+# else:
+#     print("greator than 10")
+
+c = None
+if(c is None):
+    print("yes")
+else:
+    print("no")
+
+d = [45, 2, 1]
+print(45 in d)
