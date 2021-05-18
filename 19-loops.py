870c351834d28d3d2815f3dc54d5d2665bec9fda
pythoneer-in <pythoneer.in@gmail.com>
2021-05-18 09:37:33 -0700

    loops in python

---
+++ 19-loops.py
@@ -0,0 +1,34 @@
+i = 0
+# while i < 10:
+#     print(i)
+#     i = i + 1
+
+fruits = ["Apple", "banana", "mango"]
+
+# i = 0
+# while i<len(fruits):
+#     print(fruits[i])
+#     i = i + 1
+
+# for fruit in fruits:
+#     print(fruit)
+#     if(fruit == "banana"):
+#         break
+# else:
+#     print("success")
+
+# for a in "apple":
+#     print(a)
+
+# for i in range(1, 11, 2):
+#     print(i)
+
+# for i in range(10):
+#     if(i == 5):
+#         continue
+#     print(i)
+
+for i in range(10):
+    if(i%2 != 0):
+        continue
+    print(i)
