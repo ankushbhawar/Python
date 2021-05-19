97ae901dc969044be07bb5f7d176aa6f9fe1b6b1
pythoneer-in <pythoneer.in@gmail.com>
2021-05-19 04:59:54 -0700

    object oriented programming in python

---
+++ 24-oops.py
@@ -0,0 +1,17 @@
+class Computer:
+    def __init__(self, cpu, ram):
+        self.cpu = cpu
+        self.ram = ram
+
+    def config(self):
+        print("Config is : ", self.cpu, self.ram)
+
+comp1 = Computer("i5", 16)
+comp2 = Computer("Ryzen 3", 8)
+
+
+# Computer.config(comp1)
+# Computer.config(comp2)
+
+comp1.config()
+comp2.config()
